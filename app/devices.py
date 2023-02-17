84d4d7e2fd202e2c2a620fb15702257ca2a796b5
Linh Pham <questionlp@users.noreply.github.com>
2023-02-16 19:23:52 -0800

    Add file handling and update copyright and add license information

--- app/devices.py
+++ app/devices.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 # vim: set noai syntax=python ts=4 sw=4:
+# SPDX-License-Identifier: MIT
 #
-# Copyright (c) 2022 Linh Pham
+# Copyright (c) 2022-2023 Linh Pham
+# nutters is released under the terms of the MIT License
 """Devices Configuration File Loading for Nutters Web Application"""
 import json
+from pathlib import Path
 from typing import Any, Dict
 
 
 def load_devices(devices_file_path: str = "devices.json") -> Dict[str, Dict[str, Any]]:
-    with open(devices_file_path, "r") as devices_file:
-        devices_config = json.load(devices_file)
+    if devices_file_path and Path(devices_file_path).exists():
+        with open(devices_file_path, "r") as devices_file:
+            devices_config = json.load(devices_file)
 
-    if "devices" in devices_config:
-        return devices_config["devices"]
+        if "devices" in devices_config:
+            return devices_config["devices"]
+
+    return None
--- app/mappings.py
+++ app/mappings.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 # vim: set noai syntax=python ts=4 sw=4:
+# SPDX-License-Identifier: MIT
 #
-# Copyright (c) 2022 Linh Pham
+# Copyright (c) 2022-2023 Linh Pham
+# nutters is released under the terms of the MIT License
 """Mappings File Loading for Nutters Web Application"""
 import json
+from pathlib import Path
 from typing import Any, Dict
 
 
 def load_mappings(
-    mappings_file_path: str = "mappings.json",
+    mapping_file_path: str = "mappings.json",
 ) -> Dict[str, Dict[str, Any]]:
-    with open(mappings_file_path, "r") as mappings_file:
-        mappings = json.load(mappings_file)
+    fallback_mapping_file = "mappings.json.dist"
 
-    if "mappings" in mappings:
-        return mappings["mappings"]
+    if mapping_file_path and not Path(mapping_file_path).exists():
+        if Path(fallback_mapping_file).exists():
+            mapping_file_path = fallback_mapping_file
+        else:
+            return None
+
+    if Path(mapping_file_path).exists():
+        with open(mapping_file_path, "r") as mapping_file:
+            mappings = json.load(mapping_file)
+
+        if "mappings" in mappings:
+            return mappings["mappings"]
+
+    return None
